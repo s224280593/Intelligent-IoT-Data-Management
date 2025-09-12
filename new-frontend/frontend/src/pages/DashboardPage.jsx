714fbecfcb58fc9c5a14ea85cb06309eeb08db41
Guat Teo <mtanmath@gmail.com>
2025-09-12 15:07:42 +1000

    Add DashboardLayout.jsx for Footer, Header, SideNav

--- new-frontend/frontend/src/pages/DashboardPage.jsx
+++ new-frontend/frontend/src/pages/DashboardPage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import Dashboard from '../components/Dashboard';
+import DashboardLayout from '../layouts/DashboardLayout';
 
 const DashboardPage = () => {
   const { id } = useParams(); // e.g. 'sensor1'
